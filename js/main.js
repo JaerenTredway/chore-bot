d36c79a451ea29a3ba5706dfb6184a5c77f3318f
Jaeren William Tredway <jtredway@cnm.edu>
2018-11-13 16:26:03 -0700

    Added finished code for my Codecademy project.

--- js/main.js
+++ js/main.js
@@ -2,25 +2,35 @@
 let doorImage1 = document.querySelector('#door1');
 let doorImage2 = document.querySelector('#door2');
 let doorImage3 = document.querySelector('#door3');
-const numClosedDoors = 3;
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
 let closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg'
-const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
-const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
-const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-let startButton = document.body.querySelector('#start');
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+let startButton = document.getElementById('start');
+let currentlyPlaying = true;
 
-
-playDoor = () => {
+let playDoor = (door) => {
   numClosedDoors--;
   if (numClosedDoors === 0) {
+    gameOver('win');
+  } else if (isBot(door)) {
     gameOver();
   }
 };
 
-isClicked = (door) => {
+let isBot = (door) => {
+  if (door.src === botDoorPath) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let isClicked = (door) => {
   if (door.src === closedDoorPath) {
     return false;
   } else {
@@ -28,7 +38,7 @@ isClicked = (door) => {
   }
 };
 
-randomChoreDoorGenerator = () =>
+let randomChoreDoorGenerator = () =>
 {
   let choreDoor = Math.floor(Math.random() * numClosedDoors);
   if (choreDoor === 0) {
@@ -46,30 +56,56 @@ randomChoreDoorGenerator = () =>
   }
 };
 
-if (!isClicked(doorImange1)) {
-  doorImage1.onclick = () => {
+
+doorImage1.onclick = () => {
+    if (currentlyPlaying && !isClicked(doorImage1)) {
     doorImage1.src = openDoor1;
-    isClicked(door);
-    playDoor();
+    isClicked(doorImage1);
+    playDoor(doorImage1);
   }
 };
-if (!isClicked(doorImange1)) {
-  doorImage2.onclick = () => {
+
+doorImage2.onclick = () => {
+    if (currentlyPlaying && !isClicked(doorImage2)) {
     doorImage2.src = openDoor2;
-    isClicked(door);
-    playDoor();
+    isClicked(doorImage2);
+    playDoor(doorImage2);
   }
 };
-if (!isClicked(doorImange1)) {
-  doorImage3.onclick = () => {
+
+doorImage3.onclick = () => {
+    if (currentlyPlaying && !isClicked(doorImage3)) {
     doorImage3.src = openDoor3;
-    isClicked(door);
-    playDoor();
+    isClicked(doorImage3);
+    playDoor(doorImage3);
   }
 };
 
-gameOver = () => {
-
+startButton.onclick = () => {
+   if (currentlyPlaying === false) {
+      startRound();
+   }
 };
 
+startRound = () => {
+  numClosedDoors = 3;
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  startButton.innerHTML = 'Good luck!';
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+};
+
+
+ let gameOver = (status) => {
+  if (status === 'win') {
+    startButton.innerHTML = 'You win! Play again?';
+  } else {
+    startButton.innerHTML = 'Game over! Play again?'
+  };
+  currentlyPlaying = false;
+};
+
+//startRound(); Why would you do this? Appears to be unnecessary...
 randomChoreDoorGenerator();
